6b7338b0fc8c2f379379983d1151405b9aaf3458
H-Was1 <hammadwasi.b@gmail.com>
2024-05-09 19:54:17 +0530

    new

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -28,7 +28,81 @@ const Page = async ({
   const completedFields = requiredFields.filter(Boolean).length;
   const completionText = `(${completedFields}/${totalFields})`;
 
-  return <div className="p-6"></div>;
-};
+  return (
+    <div>
+      {userProgress?.isCompleted && (
+        <Banner variant="success" label="You already completed this chapter." />
+      )}
+
+      {isLocked && (
+        <Banner
+          variant="warning"
+          label="You need to purchase this course to watch this chapter."
+        />
+      )}
+
+      <div className="flex flex-col max-w-4xl pb-20 mx-auto">
+        <div className="p-4">
+          <VideoPlayer
+            isLocked={isLocked}
+            title={chapter.title}
+            courseId={params.courseId}
+            chapterId={params.chapterId}
+            completeOnEnd={completeOnEnd}
+            nextChapterId={nextChapter?.id}
+            playbackId={muxData?.playbackId!}
+          />
+        </div>
+
+        <div>
+          <div className="flex flex-col items-center justify-between p-4 md:flex-row">
+            <h2 className="mb-2 text-2xl font-semibold">{chapter.title}</h2>
+
+            {purchase ? (
+              <CourseProgressButton
+                courseId={params.courseId}
+                chapterId={params.chapterId}
+                nextChapterId={nextChapter?.id}
+                isCompleted={!!userProgress?.isCompleted}
+              />
+            ) : (
+              <CurseEnrollButton
+                price={course.price!}
+                courseId={params.courseId}
+              />
+            )}
+          </div>
+
+          <Separator />
+
+          <div>
+            <Preview value={chapter.description!} />
+          </div>
+
+          {!!attachments.length && (
+            <>
+              <Separator />
+
+              <div className="p-4">
+                {attachments.map(attachment => (
+                  <a
+                    target="_blank"
+                    key={attachment.id}
+                    href={attachment.url}
+                    rel="noopener noreferrer nofollow external"
+                    className="flex items-center w-full p-3 border rounded-md bg-sky-200 text-sky-700 hover:underline"
+                  >
+                    <File />
+                    <p className="line-clamp-1">{attachment.name}</p>
+                  </a>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
 
 export default Page;
